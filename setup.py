c6b01748d95fa366c13e40bd9862787ab5108037
tlasky <da.tlaskal@gmail.com>
2020-12-16 17:58:13 +0100

    Added setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type='text/markdown',
     url='https://github.com/tlaskyd/tlaskyinsta',
-    packages=setuptools.find_packages(),
+    packages=['tlaskyinsta'],
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
