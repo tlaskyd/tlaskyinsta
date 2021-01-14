16682fdcfb788cb103405f68e60533ec92a2f6c1
tlasky <da.tlaskal@gmail.com>
2021-01-14 14:00:14 +0100

    Bot updates.

--- config_example.py
+++ config_example.py
@@ -1,10 +1,13 @@
+from typing import Union, List
+
 session_path = './session.pickle'
 username, password = 'your username', 'your password'
 debug = False
 
-tags = 'nature czechnature czechgirl slovaknature slovakgirl'.split(' ')
-locations = [
+interests: List[Union[str, int]] = list()
+interests.extend('nature czechnature czechgirl slovaknature slovakgirl'.split())
+interests.extend([
     244516490,  # CZ
     261698127,  # SK
     108100019211318,  # DE
-]
+])
