6a5691183917bc2618e1d19fca6abb1ebc6bf8aa
tlaskyd <da.tlaskal@gamil.com>
2021-02-25 10:31:42 +0100

    Updated some bot's stuff...

--- test_scheduler.py
+++ test_scheduler.py
@@ -1,11 +1,10 @@
 from schedule import Scheduler
 
 scheduler = Scheduler()
-scheduler.every(1).second.do(lambda: print('_'))
+scheduler.every(1).second.do(lambda: print('Executed'))
 
 try:
     while True:
-        #print('-')
         scheduler.run_pending()
 except KeyboardInterrupt:
     pass
